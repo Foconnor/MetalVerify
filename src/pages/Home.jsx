d9e43420b896949ce943caaef9b475fc4cd0719d
Mohamed-Amiin Mohamed <D00245319@student.dkit.ie>
2026-03-09 14:31:35 +0000

    Added login feature using google & Changes to Main
    As said above added the feature for loging in using google account and
    am in the process of working on making the main page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "3rem" }}>
+      <h1>Metal Verify</h1>
+
+      <p>Welcome to Metal Verify.</p>
+      <p>Check silver authenticity using sound and density testing.</p>
+
+      <div style={{ marginTop: "2rem" }}>
+        <p>Live Silver Price: Coming Soon</p>
+        <p>Silver Market News: Coming Soon</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
