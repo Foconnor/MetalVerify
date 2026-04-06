24100a22efdd00dc2224f1d4297c8adc1e776587
Mohamed <D00245319@student.dkit.ie>
2026-04-06 11:03:48 +0100

    Added Magnet Test feature
    This feature is mainly aimed to introduce the procedure to test silver
    materials using magnets. it provides a video to show the user how it is
    done.

---
+++ src/pages/MagnetPage.jsx
@@ -0,0 +1,11 @@
+import MagnetTest from '../features/magnet/magnetTest.jsx';
+
+function MagnetPage() {
+  return (
+    <div>
+      <MagnetTest />
+    </div>
+  );
+}
+
+export default MagnetPage;
